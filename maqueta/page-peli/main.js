3357cec11d49c61cf6f0daa120dd48a6ac23043f
Aitor Simon Munoz <aitor.simon@globant.com>
2023-02-16 10:15:18 +0100

    build: semantic html refactor page-pelis.html and page-peli.html

---
+++ maqueta/page-peli/main.js
@@ -0,0 +1,5 @@
+const logo = document.querySelector('#logo-container');
+
+logo.addEventListener('click', () => {
+    window.location.href = 'http://127.0.0.1:5500/maqueta/page-pelis/index.html';
+});
--- maqueta/page-pelis/main.js
+++ maqueta/page-pelis/main.js
@@ -1,10 +1,37 @@
-const films  = document.querySelectorAll('.film');
+const films = document.querySelectorAll('.film');
 const logo = document.querySelector('#logo-container');
+const iSearch = document.querySelector('input[type="search"]');
+const apiUrl = 'http://www.omdbapi.com/?apikey=ab64c929&';
+const apiKey = 'ab64c929';
+
 logo.addEventListener('click', () => {
-    window.location.href = 'http://127.0.0.1:5500/maqueta/page-pelis/index.html';
+  window.location.href = 'http://127.0.0.1:5500/maqueta/page-pelis/index.html';
 });
 films.forEach((film) => {
-    film.addEventListener('click', (e) => {
-        window.location.href = 'http://127.0.0.1:5500/maqueta/page-peli/index.html'
-	});
+  film.addEventListener('click', (e) => {
+    window.location.href = 'http://127.0.0.1:5500/maqueta/page-peli/index.html';
+  });
 });
+
+iSearch.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    sendRequestOMDB();
+  }
+});
+
+function sendRequestOMDB() {
+  let params = new Map();
+  params.set('apikey', apiKey);
+  params.set('s', document.querySelector('#search').value);
+  let jsonParams = JSON.stringify(params);
+  fetch(apiUrl+'s=disney')
+    .then((response) => {
+      document.querySelector('#search').value = '';
+      console.log(response.text);
+
+    })
+    .catch((error) => {
+      console.log(error);
+      document.querySelector('#search').value = '';
+    });
+}
